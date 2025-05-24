792b9bb78820036407049d936b546330ef33924d
Alaa <alaakhater232@gmail.com>
2025-05-24 12:46:42 +0200

    Version 2: Enhanced hover effects and updated alert message

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('alertButton').addEventListener('click', function() {
-    alert('Hello! You clicked the button!');
+    alert('Welcome to Version 2! This is an enhanced interactive button.');
 });
